39ec5d6f916b61cb958861d2018d38daa5d9248e
mingmingc <caressi.m@gmail.com>
2020-10-28 14:33:37 -0700

    list of channels retrieves all channels, next to render

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import { USER_ID } from '../const';
+import { _getSbInstance } from '../utils';
+import DashboardChatList from './DashboardChatList';
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            channels: []
+        }
         this.userId = USER_ID;
+        this.sb = _getSbInstance();
     }
-    componentDidMount() { console.log(USER_ID); }
+
+    componentDidMount() { 
+        let allUserChannels = this.sb.GroupChannel.createMyGroupChannelListQuery();
+        allUserChannels.includeEmpty = true;
+        allUserChannels.userIdsIncludeFilter = [this.userId];
+        return new Promise((resolve, reject) => {
+            allUserChannels.next((myChannels, error) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(myChannels);
+                console.log(myChannels);
+                this.setState({channels: myChannels})
+              }
+            });
+        });
+    }
+     
     render() {
         return(
             <div className="Dashboard">
-                <h1> Welcome, {this.userId} </h1> 
+                <h1> Welcome, {this.userId} </h1>
+                <DashboardChatList channels = {this.state.channels}/> 
             </div>
         )
     }
---
+++ client/src/utils.js
@@ -0,0 +1,5 @@
+import Sendbird from 'sendbird';
+
+export function _getSbInstance() {
+    return Sendbird.getInstance();
+}
